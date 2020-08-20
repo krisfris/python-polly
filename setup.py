1c61b967fde3bbfbddcdafe9e7637a113472a900
Kris <31852063+krisfris@users.noreply.github.com>
2020-08-20 12:07:44 +0300

    Prepare for publishing

--- setup.py
+++ setup.py
@@ -7,10 +7,18 @@ from setuptools import setup, find_packages
 # see https://github.com/pypa/pip/issues/7953
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='python-polly',
     version='0.1.0',
+    description='Wrapper for AWS Polly with segmentation, playback queue and client-server setup',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/krisfris/python-polly',
+    author='Kris',
+    author_email='31852063+krisfris@users.noreply.github.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
